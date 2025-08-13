62a9a1f7ac68b814be2e85fd856c4a9f96d8da48
Andres N. Salcedo <salcedo.11@osu.edu>
2025-08-13 13:11:33 -0700

    Update compute_mocks.c

--- compute_mocks.c
+++ compute_mocks.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
   if(argc != 20) {
-    printf("%d usage: ./compute_mocks Omega_m0 redshift siglogM logMmin logM0 logM1 alpha q_cen q_sat del_gamma A_con f_cen alpha_cen alpha_sat boxsize seed [halo catalog file] [galaxy mock file] [halo environment file]\n",argc);
+    printf("%d usage: ./compute_mocks Omega_m0 redshift siglogM logMmin logM0 logM1 alpha q_cen q_sat del_gamma A_con f_cen alpha_cen alpha_sat_l alpha_sat_h M_pivot_as boxsize seed [halo catalog file] [galaxy mock file] [halo environment file]\n",argc);
     return -1;
   }
 
@@ -21,33 +21,35 @@ int main(int argc, char *argv[])
   double A_con = strtod(argv[11], NULL);
   double f_cen = strtod(argv[12], NULL);
   double alpha_cen = strtod(argv[13], NULL);
-  double alpha_sat = strtod(argv[14], NULL);
+  double alpha_sat_l = strtod(argv[14], NULL); // satellite velocity bias below the pivot mass (argument below)
+  double alpha_sat_h = strtod(argv[15], NULL); // satellite velocity bias above the pivot mass
+  double M_pivot_as  = strtod(argv[16], NULL); //pivot mass for sat. vel. bias
 
-  double boxsize = strtod(argv[15], NULL);
+  double boxsize = strtod(argv[17], NULL);
 
   fprintf(stderr,"\tboxsize: %lf\n", boxsize);
 
-  int seed = atoi(argv[16]);
+  int seed = atoi(argv[18]);
 
   char *halo_file, *output_file, *env_file;
   size_t halo_file_ssize, output_file_ssize, env_file_ssize;
 
-  halo_file_ssize = sizeof(char)*(strlen(argv[17]) + 1);
-  output_file_ssize = sizeof(char)*(strlen(argv[18]) +1 );
-  env_file_ssize = sizeof(char)*(strlen(argv[19]) +1 );
+  halo_file_ssize = sizeof(char)*(strlen(argv[19]) + 1);
+  output_file_ssize = sizeof(char)*(strlen(argv[20]) +1 );
+  env_file_ssize = sizeof(char)*(strlen(argv[21]) +1 );
 
   halo_file = malloc(halo_file_ssize);
   output_file = malloc(output_file_ssize);
   env_file = malloc(env_file_ssize);
-  snprintf(halo_file, halo_file_ssize, "%s", argv[17]);
-  snprintf(output_file, output_file_ssize, "%s", argv[18]);
-  snprintf(env_file, env_file_ssize, "%s", argv[19]);
+  snprintf(halo_file, halo_file_ssize, "%s", argv[19]);
+  snprintf(output_file, output_file_ssize, "%s", argv[20]);
+  snprintf(env_file, env_file_ssize, "%s", argv[21]);
 
   fprintf(stderr,"Computing HOD from %s\n", halo_file);
   fprintf(stderr,"Reading environment density from %s\n", env_file);
   fprintf(stderr,"Saving to output file %s\n", output_file);
 
-  populate_hod(siglogM, logMmin, logM0, logM1, alpha, q_cen, q_sat, A_con, del_gamma, f_cen, alpha_cen, alpha_sat, \
+  populate_hod(siglogM, logMmin, logM0, logM1, alpha, q_cen, q_sat, A_con, del_gamma, f_cen, alpha_cen, alpha_sat_l, alpha_sat_h, M_pivot_as \
 	       seed, Omega_m0, redshift, boxsize, halo_file, output_file, env_file);
 
   return 0;
